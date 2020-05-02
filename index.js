ae48aceec1b691b636ae841870c04b7d0f1f36ad
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-05-02 17:56:13 +0900

    back-tick, DOM Control

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-console.log(console);
+// console.log(console);
 
 function hello(name, age) {
-  console.log("hello! ", name, " you have ", age, " years of age"); 
+  console.log("hello! ", name, " you have ", age, " years of age");
+  console.log(`hello! ${name} you have ${age} years of age`);
 }
 
-hello("winn", "18")
+hello("winn", "18");
+
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+};
+
+console.log(calculator.plus(5, 6));
+
+const title = document.getElementById("title");
+console.log(title);
+title.innerHTML = "hi from js";
+console.log(title);
