cb868f577e64a8fc3bd951331ec6c26f31ea292e
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-05-03 00:41:56 +0900

    how to use about className, classList, toggle

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ console.log(calculator.plus(5, 6));
 const title = document.querySelector("#title"); // # is id on html,
 // const title = document.querySelector(".title"); // dot is class on html,
 
-console.log(title);
-title.innerHTML = "hi from js";
-title.style.color = "red";
-console.log(title);
+// console.log(title);
+// title.innerHTML = "hi from js";
+// title.style.color = "red";
+// console.log(title);
 // console.dir(title); // show all element
 document.title = "I own this page";
 
@@ -31,16 +31,31 @@ function handleResize(event) {
   console.log(event);
 }
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "red";
+// const BASE_COLOR = "rgb(52, 73, 94)";
+// const OTHER_COLOR = "red";
+
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-  }
+  // const currentColor = title.style.color;
+  // if (currentColor === BASE_COLOR) {
+  //   title.style.color = OTHER_COLOR;
+  // } else {
+  //   title.style.color = BASE_COLOR;
+  // }
+
+  /**
+   * this logic can change -> toggle function
+   */
+  // const hasClass = title.classList.contains(CLICKED_CLASS);
+  // if (!hasClass) {
+  //   // title.className = CLICKED_CLASS
+  //   title.classList.add(CLICKED_CLASS);
+  // } else {
+  //   title.classList.remove(CLICKED_CLASS);
+  // }
+
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function handleOnline() {
@@ -52,7 +67,7 @@ function handleOffline() {
 }
 
 function init() {
-  title.style.color = BASE_COLOR;
+  // title.style.color = BASE_COLOR;
   // window.addEventListener("resize", handleResize()); // 바로 실행
   window.addEventListener("resize", handleResize); // 이벤트가 발생할때 실행
   title.addEventListener("click", handleClick);
