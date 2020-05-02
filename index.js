5a8cd9687d37727c9021836213d65ff425f61559
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-05-02 18:25:37 +0900

    what is diff ? each of function call name format

--- index.js
+++ index.js
@@ -15,7 +15,26 @@ const calculator = {
 
 console.log(calculator.plus(5, 6));
 
-const title = document.getElementById("title");
+// const title = document.getElementById("title");
+const title = document.querySelector("#title"); // # is id on html,
+// const title = document.querySelector(".title"); // dot is class on html, 
+
 console.log(title);
 title.innerHTML = "hi from js";
+title.style.color = "red";
 console.log(title);
+// console.dir(title); // show all element
+document.title = "I own this page";
+
+function handleResize(event) {
+  // console.log("have beed resized");
+  console.log(event);
+}
+
+function handleClick() {
+  title.style.color = "blue";
+}
+
+// window.addEventListener("resize", handleResize()); // 바로 실행
+window.addEventListener("resize", handleResize); // 이벤트가 발생할때 실행
+title.addEventListener("click", handleClick);
