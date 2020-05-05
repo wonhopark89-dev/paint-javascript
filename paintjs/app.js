ba105a181ea905e028bb6a7d061d6b8be08a7abf
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-05-06 00:35:44 +0900

    canvas fillRect, fillStyle

--- paintjs/app.js
+++ paintjs/app.js
@@ -1,13 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor"); // array
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 600;
-canvas.height = 600;
-ctx.strokeStyle = "#2c2c2c";
+const INIT_COLOR = "#2C2C2C";
+const CANVAS_SIZE = 600;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INIT_COLOR;
+ctx.fillStyle = INIT_COLOR;
 ctx.lineWidth = "2.5";
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -44,15 +52,49 @@ function onMouseLeave(event) {
 
 function handleColorClick(event) {
   // console.log(event);
-  ctx.strokeStyle = event.target.style.backgroundColor;
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
+
+function handleRangeChange(event) {
+  console.log(event.target.value);
+  ctx.lineWidth = event.target.value;
+}
+
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", onMouseDown);
   canvas.addEventListener("mouseup", onMouseUp);
   canvas.addEventListener("mouseleave", onMouseLeave);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  // 일종의 방어코드
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
