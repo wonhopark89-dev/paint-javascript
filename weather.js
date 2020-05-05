e5887118f779245533771947b9225215096096f7
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-05-05 20:36:30 +0900

    add openweather api, fetch

--- weather.js
+++ weather.js
@@ -1,4 +1,22 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "58d07c2d6dade6e696cf17aaf7951f56";
 const COORDS = "coords";
+
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json(); // response 중 필요한 데이터만 json 으로 바꾼 후 진행하기 위해
+    })
+    .then(function (json) {
+      // console.log(json);
+      const temp = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temp} / ${place}`;
+    });
+}
 
 function saveCoords(coords) {
   localStorage.setItem(COORDS, JSON.stringify(coords));
@@ -13,6 +31,7 @@ function handleGeoSuccess(position) {
   };
 
   saveCoords(coords);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -28,6 +47,9 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    // console.log(parseCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 function init() {
