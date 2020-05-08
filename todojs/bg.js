50bfb42951f183a8265b81344f731a1cc43109a2
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-05-08 23:59:17 +0900

    move folder

--- todojs/bg.js
+++ todojs/bg.js
@@ -5,7 +5,7 @@ const IMG_NUMBER = 3;
 function paintImage(imgNumber) {
   console.log(imgNumber);
   const image = new Image();
-  image.src = `images/${imgNumber}.jpg`;
+  image.src = `../images/${imgNumber}.jpg`;
   image.classList.add("bgImage"); // css 속성
   body.prepend(image);
 }
